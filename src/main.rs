480d212b3002a256b7848e781ad63071efde4950
Guileas <ethevegwenola@gmail.com>
2022-09-01 00:59:39 +0200

    feat: create a function to generate the square game board

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rand::prelude::*;
 use crossterm::{event::{read, Event, KeyCode, KeyEvent, KeyModifiers, KeyEventKind, KeyEventState}};
 
 
+
 fn main(){
     println!("                                                                                               
     _|_|_|_|_|                                                    _|  _|  _|    _|                
@@ -16,8 +17,6 @@ fn main(){
     loop {
         keyword_event(read().unwrap());
     }
-
-    
 }
 
 // Read keyword event and execute a function if necessary
@@ -53,7 +52,52 @@ pub fn shuffle(mut cards: [i8;85]) -> [i8;85] {
 }
 
 pub fn setup() {
-    let mut island_map: [i8; 85] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,100,100,100,100,100];
+    let island_stack: [i8; 85] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,100,100,100,100,100];
 
-    island_map = shuffle(island_map);
+    // Create and Display the board game
+    let raw_board = create_square_board();
+   
+
+    // Shuffle the stack and create the user hand
+    let player_deck = shuffle(island_stack);
+}
+
+// Prepare the array use to construct the square board
+fn create_square_board() -> [[String; 8]; 8] {
+    
+    // The first map is a square of 8x8
+    let max_square_size = 8;
+    let mut map: [[String; 8]; 8] =  Default::default();
+
+    for i in 0..max_square_size {
+        for j in 0..max_square_size {
+            // First row
+            if i == 0 && j == 0 { map[i][j] = "_".to_string(); }
+            else if i == 0 && j != 0 && j != 7 {
+                map[i][j] = String::from(&j.to_string());
+            } 
+            else if i == 0 && j == 7 { map[i][j] = "x".to_string(); }
+
+            // First column
+            else if j == 0 && i != 0 && i != 7 { 
+                map[i][j] = String::from(&i.to_string());
+            }
+
+            // Last column
+            else if j == 7 && i != 0 && i != 7 { 
+                map[i][j] = String::from(&i.to_string());
+            }
+
+            // Last row
+            else if i == 7 && j == 0 { map[i][j] = "s".to_string(); }
+            else if i == 7 && j != 0 && j != 7 {
+                map[i][j] = String::from(&j.to_string());
+            } 
+            else if i == 7 && j == 7 { map[i][j] = "_".to_string(); }
+
+            // Board
+            else { map[i][j] = "\x1b[93m*\x1b[0m".to_string(); }
+        }   
+    }
+    return map;
 }
